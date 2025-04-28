1b2e54772e7d32c3c8189278366e54526e31c71c
sum-young <summeryoung@ewhain.net>
2025-04-28 23:52:27 +0900

    docs: add 과제&회고 week2

---
+++ week2/15 박서영/변수선언.ts
@@ -0,0 +1,11 @@
+//1.변수선언: 이름, 나이, 합격여부 변수 타입 명시해서 선언하기
+
+//타입: string-문자열, number -정수/실수, boolean-참/거짓
+let 이름 : string = '박서영';
+let 나이 : number = 22;
+let 합격여부 : boolean = true;
+
+//console.log -> print같은 느낌?
+console.log(이름);
+console.log(나이);
+console.log(합격여부);
