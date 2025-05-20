c41fdeef7e2ccfd2e7c7a5d42ede485ec68fd63b
sum-young <summeryoung@ewhain.net>
2025-05-20 14:15:26 +0900

    Revert "[week3] 15박서영 3주차 과제 제출"
    This reverts commit 7fbd264bba3439d5c3ee2518ce2a4ec4c78b3d73.

--- week3/15 박서영/object.ts
+++
@@ -1,9 +0,0 @@
-let Person = {
-    name: "박서영",
-    age: 22,
-    email: "summeryoung@ewhain.net"
-}
-
-console.log(`이름: ${Person.name}`);
-console.log(`나이: ${Person.age}`);
-console.log(`이메일: ${Person.email}`);
